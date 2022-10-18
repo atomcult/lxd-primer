657e385fa9f0552c92781575da4b87db60dd9d13
jmbrock <joseph.brock@canonical.com>
2022-10-17 21:41:55 -0400

    Create a script to copy configs in lieu of a mount

---
+++ provision.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+HERE="$(readlink -f "$0" | xargs dirname)"
+
+if [ ! -d /dev/lxd ]; then
+    echo    "This is meant to be run in an LXD container."
+    echo -n "Are you sure you want to continue? (type: YES) "
+
+    read RESPONSE
+    if [ "$RESPONSE" != "YES" ]; then
+        echo
+        echo Aborting.
+        exit 1
+    fi
+fi
+
+cp "${HERE}"/dot-config/* "${XDG_CONFIG_HOME:-$HOME/.config}" \
+    --verbose \
+    --recursive \
+    --interactive
+
+cp "${HERE}"/dot-vim/* "$HOME/.vim" \
+    --verbose \
+    --recursive \
+    --interactive
